d307e8c42e0fc3566b432e167b41053849a438e1
mohammedbahnini <mohammed.gosoft@gmail.com>
2024-05-28 10:05:03 +0100

    move loading elements to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
-import React, { Suspense, lazy } from "react"
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Subscribe = lazy(() => import('./pages/Subscribe'));
-import Layout from "./pages/Layout"
-import SuspensePage from "./pages/SuspensePage";
-import ErrorPage from "./pages/ErrorPage";
+import React, {  lazy } from "react"
+import { router } from "./main"
+import { RouterProvider } from "react-router-dom"
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<SuspensePage page={<Home />} />} />
-        <Route path="about" element={<SuspensePage page={<About />} />} />
-        <Route path="subscribe" element={<SuspensePage page={<Subscribe />} />} />
-      </Route>
-
-      <Route path="*" element={<SuspensePage page={<ErrorPage />} />} />
-    </Route>
-  )
-)
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Subscribe = lazy(() => import('./pages/Subscribe'));
+import Layout from "./pages/Layout"
+import SuspensePage from "./pages/SuspensePage";
+import ErrorPage from "./pages/ErrorPage";
+
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<SuspensePage page={<Home />} />} />
+        <Route path="about" element={<SuspensePage page={<About />} />} />
+        <Route path="subscribe" element={<SuspensePage page={<Subscribe />} />} />
+      </Route>
+
+      <Route path="*" element={<SuspensePage page={<ErrorPage />} />} />
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
